55968badcf9371d9383a49f661c18d63738fe3e0
Josh Williams <josh@spacesnottabs.com>
2022-07-09 10:21:00 +0000

    Updated Elasticsearch connection to specify protocol

--- lib/register_sources_oc/config/elasticsearch.rb
+++ lib/register_sources_oc/config/elasticsearch.rb
@@ -7,8 +7,8 @@ module RegisterSourcesOc
     raise MissingEsCredsError unless ENV['ELASTICSEARCH_HOST']
 
     ELASTICSEARCH_CLIENT = Elasticsearch::Client.new(
-      host: "http://elastic:#{ENV['ELASTICSEARCH_PASSWORD']}@#{ENV['ELASTICSEARCH_HOST']}:#{ENV['ELASTICSEARCH_PORT']}",
-      transport_options: { ssl: { verify: false } },
+      host: "#{ENV.fetch('ELASTICSEARCH_PROTOCOL', 'http')}://elastic:#{ENV['ELASTICSEARCH_PASSWORD']}@#{ENV['ELASTICSEARCH_HOST']}:#{ENV['ELASTICSEARCH_PORT']}",
+      transport_options: { ssl: { verify: ENV.fetch('ELASTICSEARCH_SSL_VERIFY', false) } },
       log: false
     )
   end
