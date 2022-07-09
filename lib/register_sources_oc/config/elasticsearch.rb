9d185d0f0f466a7de66f19d4eb9295842ad6aaed
Josh Williams <josh@spacesnottabs.com>
2022-07-09 10:26:07 +0000

    Updated SSL verification

--- lib/register_sources_oc/config/elasticsearch.rb
+++ lib/register_sources_oc/config/elasticsearch.rb
@@ -8,7 +8,7 @@ module RegisterSourcesOc
 
     ELASTICSEARCH_CLIENT = Elasticsearch::Client.new(
       host: "#{ENV.fetch('ELASTICSEARCH_PROTOCOL', 'http')}://elastic:#{ENV['ELASTICSEARCH_PASSWORD']}@#{ENV['ELASTICSEARCH_HOST']}:#{ENV['ELASTICSEARCH_PORT']}",
-      transport_options: { ssl: { verify: ENV.fetch('ELASTICSEARCH_SSL_VERIFY', false) } },
+      transport_options: { ssl: { verify: (ENV.fetch('ELASTICSEARCH_SSL_VERIFY', false) == 'true') } },
       log: false
     )
   end
